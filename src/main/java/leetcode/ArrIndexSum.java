1f2e9b84876a67963d116baa67824dc9c338afda
a815784251 <815784251@qq.com>
2021-06-06 16:19:38 +0800

    代码提交

--- src/main/java/leetcode/ArrIndexSum.java
+++ src/main/java/leetcode/ArrIndexSum.java
@@ -21,6 +21,14 @@ public class ArrIndexSum {
         }
     }
 
+    /**
+     * 数组下2个指定元素相加等于指定数 返回下标
+     * 思路：遍历数组，map以当前下标元素距离目标的差值作为key,value存储下标。
+     * 如果map包含了当前下标的元素，则证明已经找到，返回对于的下标
+     * @param nums 数组
+     * @param target 目标数
+     * @return 下标
+     */
     public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>(16);
         for (int i = 0; i < nums.length; i++) {
